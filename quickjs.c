2795acad250682c59657c0a0322ba0c69d714fae
ekibun <soekibun@gmail.com>
2020-09-21 14:37:39 +0800

    fix build in android

--- quickjs.c
+++ quickjs.c
@@ -64,7 +64,9 @@ int gettimeofday(struct timeval * tp, struct timezone * tzp)
 
 #else
 #include <sys/time.h>
-#defind INFINITY 1.0 / 0.0
+#ifndef INFINITY
+#define INFINITY 1.0 / 0.0
+#endif
 #endif
 
 #include "cutils.h"
@@ -1707,7 +1709,7 @@ static inline size_t js_def_malloc_usable_size(void *ptr)
     return malloc_size(ptr);
 #elif defined(_WIN32)
     return _msize(ptr);
-#elif defined(EMSCRIPTEN)
+#elif defined(EMSCRIPTEN) || defined(__ANDROID__)
     return 0;
 #elif defined(__linux__)
     return malloc_usable_size(ptr);
@@ -1781,7 +1783,7 @@ static const JSMallocFunctions def_malloc_funcs = {
     malloc_size,
 #elif defined(_WIN32)
     (size_t (*)(const void *))_msize,
-#elif defined(EMSCRIPTEN)
+#elif defined(EMSCRIPTEN) || defined(__ANDROID__)
     NULL,
 #elif defined(__linux__)
     (size_t (*)(const void *))malloc_usable_size,
