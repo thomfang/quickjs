50daf8e1a272edf978aa34862c4133cca2280e0a
ekibun <soekibun@gmail.com>
2021-01-10 16:28:42 +0800

    fix MSVC build release

--- quickjs.c
+++ quickjs.c
@@ -40,6 +40,9 @@
 #endif
 
 #ifdef _MSC_VER
+#pragma function (ceil)
+#pragma function (floor)
+
 #include <WinSock2.h>
 
 // From: https://stackoverflow.com/a/26085827
@@ -1615,8 +1618,7 @@ static inline BOOL js_check_stack_overflow(JSRuntime *rt, size_t alloca_size)
 static inline uintptr_t js_get_stack_pointer(void)
 {
 #ifdef _MSC_VER
-    uint8_t ptr;
-    return &ptr;
+    return _AddressOfReturnAddress();
 #else
     return (uintptr_t)__builtin_frame_address(0);
 #endif
